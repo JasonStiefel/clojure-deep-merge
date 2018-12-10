60bb13d9cfdcb880c43d15c547f42ccb5680df89
RacecarBTF <Jason.R.Stiefel@gmail.com>
2018-12-09 21:50:21 -0800

    Bumping project version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clojure-deep-merge "0.0.1"
+(defproject clojure-deep-merge "0.1.0"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
