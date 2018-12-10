8053f0b4cba99ae509ea32b32b85a5ba1800f00d
RacecarBTF <Jason.R.Stiefel@gmail.com>
2018-12-09 22:11:38 -0800

    Updating the project file

--- project.clj
+++ project.clj
@@ -1,6 +1,5 @@
 (defproject clojure-deep-merge "0.1.0"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :url "https://github.com/JasonStiefel/clojure-deep-merge"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]]
